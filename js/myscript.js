10763ff03381d08e4aaeba82ad51b08c08834c09
Rex Luangco <rexluangco@gmail.com>
2018-03-07 21:26:10 +0800

    mar7

---
+++ js/myscript.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  $(window).scroll(function() {
+    var scroll = $(window).scrollTop();
+    if (scroll > 300) {
+      $(".black").css("background", "blue");
+    } else {
+      $(".black").css("background", "#333");
+    }
+  });
+});
